bf37f4073b59f6edce5720cf28cb0eec0d96d6c2
Sofiaguzman1 <Sofia.pimentel@newhorizons.edu.do>
2021-12-01 16:49:14 +0000

    lists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Boton1Component } from './boton1/boton1.component';
+import { Boton2Component } from './boton2/boton2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Boton1Component,
+    Boton2Component
   ],
   imports: [
     BrowserModule,
